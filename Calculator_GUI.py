33b613bbd36e4b8d7daaa065cb734f550f33a9a5
Marian Andreev <mariankata@gmail.com>
2017-11-20 21:23:32 +0100

    used grid to align calculator tiles

--- Calculator_GUI.py
+++ Calculator_GUI.py
@@ -52,34 +52,46 @@ def divideB_click():
         return
 
 
+# label 1
+l1 = Label(window, text="Enter first number here:")
+l1.grid(row=0, sticky=E)
+
+
+# label 2
+l2 = Label(window, text="Enter second number here:")
+l2.grid(row=1, sticky=E)
+
+
 # field 1
 f1 = Entry(window)
-f1.pack()
-
-
-# + button
-plusB = Button(window, text="+", command=plusB_click)
-plusB.pack()
-
-
-# - button
-minusB = Button(window, text="-", command=minusB_click)
-minusB.pack()
-
-
-# * button
-timesB = Button(window, text="*", command=timesB_click)
-timesB.pack()
-
-
-# / button
-divideB = Button(window, text="/", command=divideB_click)
-divideB.pack()
+f1.grid(row=0, column=1)
 
 
 # field 2
 f2 = Entry(window)
-f2.pack()
+f2.grid(row=1, column=1)
+
+
+# + button
+plusB = Button(window, text="+", command=plusB_click)
+plusB.grid(row=0, column=2)
+
+
+# - button
+minusB = Button(window, text="-", command=minusB_click)
+minusB.grid(row=1, column=2)
+
+
+# * button
+timesB = Button(window, text="*", command=timesB_click)
+timesB.grid(row=0, column=3)
+
+
+# / button
+divideB = Button(window, text="/", command=divideB_click)
+divideB.grid(row=1, column=3)
+
+
 
 
 
